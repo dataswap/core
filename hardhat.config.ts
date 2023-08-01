7d1a2f2d6b50f32dd49ab7331e55c6c77d164618
wayne yang <wayneyang@waynedeMacBook-Pro.local>
2023-08-01 18:29:10 +0800

    hardhat init

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+  paths: {
+    artifacts: './artifacts',
+    cache: './cache-hardhat',
+    sources: './src',
+    tests: './test',
+  },
+};
+
+export default config;
