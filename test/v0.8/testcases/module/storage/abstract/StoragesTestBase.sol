011fd8453f1f4fb863850d80644926139e42a8e9
LoveL <lovel888@hotmail.com>
2023-08-31 12:04:50 +0800

    fix: Implement storage unit test[#45]

--- test/v0.8/testcases/module/storage/abstract/StoragesTestBase.sol
+++ test/v0.8/testcases/module/storage/abstract/StoragesTestBase.sol
@@ -18,6 +18,7 @@ pragma solidity ^0.8.21;
 
 import {Test} from "forge-std/Test.sol";
 import {TestCaseBase} from "test/v0.8/testcases/module/abstract/TestCaseBase.sol";
+import {Generator} from "test/v0.8/helpers/utils/Generator.sol";
 import {IStorages} from "src/v0.8/interfaces/module/IStorages.sol";
 import {IStoragesAssertion} from "test/v0.8/interfaces/assertions/module/IStoragesAssertion.sol";
 import {IStoragesHelpers} from "test/v0.8/interfaces/helpers/module/IStoragesHelpers.sol";
@@ -27,20 +28,25 @@ import {IStoragesHelpers} from "test/v0.8/interfaces/helpers/module/IStoragesHel
 /// The `before` function is used for test case setup, and the `action` function performs the main action of the test case.
 /// The `after_` function can be used for cleanup or post-action code.
 abstract contract StoragesTestBase is TestCaseBase, Test {
+    Generator internal generator;
+
     IStorages internal storages;
     IStoragesHelpers internal storagesHelpers;
     IStoragesAssertion internal storagesAssertion;
 
     /// @dev Constructor to initialize the StoragesTestBase with the required contracts.
     /// @param _storages The address of the IStorages contract.
+    /// @param _generator The random generator contract.
     /// @param _storagesHelpers The address of the IStoragesHelpers contract.
     /// @param _storagesAssertion The address of the IStoragesAssertion contract.
     constructor(
         IStorages _storages,
+        Generator _generator,
         IStoragesHelpers _storagesHelpers,
         IStoragesAssertion _storagesAssertion
     ) {
         storages = _storages;
+        generator = _generator;
         storagesHelpers = _storagesHelpers;
         storagesAssertion = _storagesAssertion;
     }
